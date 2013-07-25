7a1e64c740db533a947d83c1143c43563da212a9
ronnieravelo <ronnieravelo@yahoo.com>
2013-07-25 14:18:55 +0000

    modified web.js to read from file

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var rbuf = fs.readFileSync("index.html");
+var jstr = JSON.stringify(rbuf); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('Hello World 2!' + jstr);
 });
 
 var port = process.env.PORT || 5000;
